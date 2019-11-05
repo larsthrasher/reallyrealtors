87cad5e076911cb8bbad70bcabcb6db048578574
larsthrasher <thrasherlars@gmail.com>
2019-11-04 20:11:11 -0500

    Added local settings import

--- rr/settings.py
+++ rr/settings.py
@@ -148,3 +148,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER='reallyrealtorz@gmail.com'
 EMAIL_HOST_PASSWORD='Re@llyre@ltors'
 EMAIL_USE_TLS=True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
